3d091bf3dc24698ae6144540ca6fbe91da005654
Piotr Wachala <piotr.wachala@outlook.com>
2018-01-04 11:13:24 +0100

    Scheduling - job executed in 10 seconds intervals

--- src/main/java/batch/service/config/BatchConfiguration.java
+++ src/main/java/batch/service/config/BatchConfiguration.java
@@ -4,11 +4,12 @@ import batch.service.listener.JobCompletionNotificationListener;
 import batch.service.model.ParkingLotEvent;
 import batch.service.processor.ParkingLotEventProcessor;
 import batch.service.service.ParkingLotService;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import lombok.extern.java.Log;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
@@ -20,12 +21,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.sql.DataSource;
+import java.time.LocalDateTime;
 
 @Configuration
 @EnableBatchProcessing
+@Log
 public class BatchConfiguration {
+
+    @Autowired
+    private JobLauncher jobLauncher;
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
@@ -38,6 +45,21 @@ public class BatchConfiguration {
 
     @Autowired
     private ParkingLotService parkingLotService;
+
+    @Autowired
+    private JobCompletionNotificationListener jobCompletionNotificationListener;
+
+    @Scheduled(fixedDelay = 10000)
+    public void perform() throws Exception {
+        log.info("Job Started at :" + LocalDateTime.now());
+
+        JobParameters param = new JobParametersBuilder().addString("JobID",
+                String.valueOf(System.currentTimeMillis())).toJobParameters();
+
+        JobExecution execution = jobLauncher.run(processParkingLotEventJob(jobCompletionNotificationListener), param);
+
+        log.info("Job finished with status :" + execution.getStatus());
+    }
 
     @Bean
     public FlatFileItemReader<ParkingLotEvent> reader() {
@@ -67,13 +89,12 @@ public class BatchConfiguration {
                 "(:parkingLotId, :eventType, :spots)");
         writer.setDataSource(dataSource);
 
-
         return writer;
     }
 
     @Bean
-    public Job processParkingLotEvent(JobCompletionNotificationListener listener) {
-        return jobBuilderFactory.get("processParkingLotEvent")
+    public Job processParkingLotEventJob(JobCompletionNotificationListener listener) {
+        return jobBuilderFactory.get("processParkingLotEventJob")
                 .incrementer(new RunIdIncrementer())
                 .listener(listener)
                 .flow(step())
